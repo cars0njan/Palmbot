6d1f254ec720c9006813efe7955975fe2b3c0b83
cars0njan <102021218+cars0njan@users.noreply.github.com>
2023-10-02 16:13:33 +0000

    ping fuction

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-
 import os
 
 import discord
+from discord import app_commands
+from discord.ext import commands
 
 import alive
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='=',intents=intents)
+
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.tree.sync()
 
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(msg):
+    if msg.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if msg.content.startswith('$Palmbot$'):
+        await msg.channel.send('Hello!')
 
+@client.tree.command(name='ping', description='Show bot ping and check if bot is online.')
+async def ping(interaction: discord.Interaction):
+    latency = round(client.latency * 1000)
+    await interaction.response.send_message(f'**{latency}** ms. Bot is online.',ephemeral=True)
 
 try:
     token = os.getenv("TOKEN") or ""
